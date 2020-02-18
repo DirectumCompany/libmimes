70d1f0015c9179995f34533f569785ff4752745f
NT_WORK\Shestakov_NV <shestakov_nv@directum.ru>
2020-02-18 18:27:24 +0400

    Внешний метод get_mime_type

--- src/library.cpp
+++ src/library.cpp
@@ -3,7 +3,9 @@
 #include "helpers/file.hpp"
 #include "exceptions/file_not_found_exception.hpp"
 
-std::string get_mime_type(const std::string &file_name) {
-    if (!helpers::file::is_exists(file_name))
-        throw exceptions::file_not_found_exception("File not found: " + file_name);
+std::string get_mime_type(const std::string &file_path) {
+    if (!helpers::file::is_exists(file_path))
+        throw exceptions::file_not_found_exception(file_path);
+
+    return helpers::file::get_mime_type(file_path);
 }
--- src/library.hpp
+++ src/library.hpp
@@ -6,10 +6,10 @@
 /**
  * Получить MIME-type указанного файла.
  *
- * @param file_name Абсолютный путь к файлу.
+ * @param file_path Абсолютный путь к файлу.
  * @return MIME-type файла.
  * @throws exceptions::file_not_found_exception
  */
-std::string get_mime_type(const std::string &file_name);
+std::string get_mime_type(const std::string &file_path);
 
 #endif //MIMES_LIBRARY_HPP
