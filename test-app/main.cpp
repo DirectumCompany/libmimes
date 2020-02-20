da06acba49ecad239a6693f86f9e3a1443c5eadc
lanorfirith <lanorfirith@gmail.com>
2020-02-20 12:59:09 +0400

    В тестовое приложение добавлено получение имени файла из параметров командной строки.

--- test-app/main.cpp
+++ test-app/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "library.hpp"
 
-int main() {
-    auto mime_type = get_mime_type("test.txt");
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Please, specify file path.";
+        return 1;
+    }
+
+    auto mime_type = get_mime_type(argv[1]);
     auto associations = get_mime_type_associations(mime_type);
     std::cout << mime_type << ": " << associations[0] << std::endl;
+    return 0;
 }
